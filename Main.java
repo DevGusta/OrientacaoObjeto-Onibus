c2f8be82c81f73c30ffc34105cc49ffa2faa10eb
Gustavo Santos <63744590+DevGusta@users.noreply.github.com>
2021-05-06 21:59:06 -0300

    Finalizando a classe e criando menu

--- Main.java
+++ Main.java
@@ -1,6 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Onibus onibus = new Onibus(40,20,10);
+        int pessoas = 0;
+        boolean continua = true;
+        boolean paradoNoPonto;
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Bem vindo a sua vigaem!");
+
+        while (continua){
+            onibus.setPonto();
+            if (onibus.getPonto() == 1){
+                System.out.println();
+                System.out.println(onibus.toString());
+            }
+
+            System.out.println();
+            System.out.println("Estamos no ponto " + onibus.getPonto());
+
+            if (pessoas > 0){
+                for (int i = 0; i < pessoas; i++) {
+                    onibus.sair();
+                }
+                System.out.println("Desceu do ônibus: " + pessoas + " pessoa(s)");
+                pessoas = 0;
+            }
+
+            paradoNoPonto = true;
+
+            while (paradoNoPonto){
+                System.out.println();
+                System.out.println("1.Entrar passageiro");
+                System.out.println("2.Pedir para sair no próximo ponto");
+                System.out.println("3.Continuar para o próximo ponto");
+                System.out.println("4.Informações sobre a viagem");
+                System.out.println("5.Encerrar viagem");
+                System.out.print("Escolha uma das opções:");
+
+                switch (teclado.nextInt()){
+                    case 1:
+                        System.out.println();
+                        System.out.print("Quantos passageiros irão entrar? ");
+                        int qtdPassageiros = teclado.nextInt();
+                        for (int i = 0; i < qtdPassageiros; i++) {
+                            onibus.entrar();
+                        }
+                        break;
+                    case 2:
+                        if(onibus.getDentroVeiculo() == 0){
+                            System.out.println();
+                            System.out.println("Não há passageiros no ônibus.");
+                            break;
+                        }
+                        System.out.println();
+                        pessoas = onibus.pedirPraDescer();
+                        break;
+                    case 3:
+                        paradoNoPonto = false;
+                        break;
+                    case 4:
+                        System.out.println();
+                        System.out.println(onibus.toString());
+                        break;
+                    case 5:
+                        paradoNoPonto = false;
+                        continua = false;
+                        break;
+                    default:
+                        System.out.println();
+                        System.out.print("Erro. Escolha uma das opções:");
+                        break;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Obrigado pela viagem!");
     }
 }
--- Onibus.java
+++ Onibus.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Onibus {
     private int dentroVeiculo;
     private int bilheteNormal;
@@ -9,6 +11,9 @@ public class Onibus {
     private int capPessoasPe;
     private int capPessoasSentadas;
     private int qtdPontos;
+    private int totalBilhetes;
+    private int voltas;
+    private int ponto;
 
     public Onibus(int capPessoasPe, int capPessoasSentadas, int qtdPontos) {
         this.capPessoasPe = capPessoasPe;
@@ -19,26 +24,176 @@ public class Onibus {
         bilheteNormal = 0;
         bilheteEstudante = 0;
         bilheteIdoso = 0;
+        totalBilhetes = 0;
         pessoasPe = 0;
         pessoasSentadas = 0;
+        voltas = 0;
+        ponto = 0;
     }
 
-    private boolean estaCheio(){
+    public int getDentroVeiculo() {
+        return dentroVeiculo;
+    }
+
+    public void setDentroVeiculo(int dentroVeiculo) {
+        this.dentroVeiculo = dentroVeiculo;
+    }
+
+    public int getBilheteNormal() {
+        return bilheteNormal;
+    }
+
+    public void setBilheteNormal(int bilheteNormal) {
+        this.bilheteNormal = bilheteNormal;
+    }
+
+    public int getBilheteIdoso() {
+        return bilheteIdoso;
+    }
+
+    public void setBilheteIdoso(int bilheteIdoso) {
+        this.bilheteIdoso = bilheteIdoso;
+    }
+
+    public int getBilheteEstudante() {
+        return bilheteEstudante;
+    }
+
+    public void setBilheteEstudante(int bilheteEstudante) {
+        this.bilheteEstudante = bilheteEstudante;
+    }
+
+    public int getPessoasPe() {
+        return pessoasPe;
+    }
+
+    public void setPessoasPe(int pessoasPe) {
+        this.pessoasPe = pessoasPe;
+    }
+
+    public int getPessoasSentadas() {
+        return pessoasSentadas;
+    }
+
+    public void setPessoasSentadas(int pessoasSentadas) {
+        this.pessoasSentadas = pessoasSentadas;
+    }
+
+    public int getCapTotal() {
+        return capTotal;
+    }
+
+    public void setCapTotal(int capTotal) {
+        this.capTotal = capTotal;
+    }
+
+    public int getCapPessoasPe() {
+        return capPessoasPe;
+    }
+
+    public void setCapPessoasPe(int capPessoasPe) {
+        this.capPessoasPe = capPessoasPe;
+    }
+
+    public int getCapPessoasSentadas() {
+        return capPessoasSentadas;
+    }
+
+    public void setCapPessoasSentadas(int capPessoasSentadas) {
+        this.capPessoasSentadas = capPessoasSentadas;
+    }
+
+    public int getQtdPontos() {
+        return qtdPontos;
+    }
+
+    public void setQtdPontos(int qtdPontos) {
+        this.qtdPontos = qtdPontos;
+    }
+
+    public int getTotalBilhetes() {
+        return totalBilhetes;
+    }
+
+    public void setTotalBilhetes(int totalBilhetes) {
+        this.totalBilhetes = totalBilhetes;
+    }
+
+    public int getVoltas() {
+        return voltas;
+    }
+
+    public void setVoltas() {
+        voltas++;
+
+    }
+
+    public int getPonto() {
+        return ponto;
+    }
+
+    public void setPonto() {
+        if (ponto >= 10){
+            setVoltas();
+            ponto = 0;
+        }
+        ponto++;
+    }
+
+    public boolean estaCheio(){
         return capTotal == pessoasPe + pessoasSentadas;
     }
 
-    private void Entrar(){
+    public void entrar(){
         if(!estaCheio()){
             if(pessoasSentadas != capPessoasSentadas){
                 pessoasSentadas++;
             }else{
                 pessoasPe++;
             }
+
+            System.out.println("1.Bilhete Normal");
+            System.out.println("2.Bilhete de Idoso");
+            System.out.println("3.Bilhete de Estudante");
+            System.out.println();
+            System.out.print("Digite o tipo de bilhete do passageiro:");
+
+            Scanner teclado = new Scanner(System.in);
+
+            boolean continua = true;
+
+            while (continua){
+                int tipoBilhete = teclado.nextInt();
+                switch (tipoBilhete){
+                    case 1:
+                        bilheteNormal++;
+                        continua = false;
+                        break;
+                    case 2:
+                        bilheteIdoso++;
+                        continua = false;
+                        break;
+                    case 3:
+                        bilheteEstudante++;
+                        continua = false;
+                        break;
+                    default:
+                        System.out.println();
+                        System.out.println("Erro. Escolha uma das opções.");
+                        System.out.println("Digite o tipo de bilhete:");
+                        break;
+
+                }
+            }
+
             dentroVeiculo++;
+            totalBilhetes++;
+        }else {
+            System.out.println("O ônibus está cheio.");
         }
     }
 
-    private  void Sair(){
+    public void sair(){
         if (dentroVeiculo != 0){
             if (pessoasPe > 0){
                 pessoasPe --;
@@ -49,4 +204,33 @@ public class Onibus {
         }
     }
 
+    public void contarNumerosBilhetes(){
+        int bilhetes = getTotalBilhetes();
+        System.out.println("Total de passageiros até o momento: " + bilhetes);
+    }
+
+    public int pedirPraDescer(){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Quantas pessoas irão descer no próximo ponto ?");
+        return teclado.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        return "Informações:" +
+                "\nQuantidade de pontos na viagem: " + qtdPontos +
+                "\nPonto atual: " + ponto +
+                "\nPassageiros no ônibus: " + dentroVeiculo +
+                "\nCapacidade total do ônibus: " + capTotal +
+                "\nPassageiros sentados ônibus: "  + pessoasSentadas +
+                "\nCapacidade total de pessoas sentadas do ônibus: "  + capPessoasSentadas +
+                "\nPassageiros em pé ônibus: " + pessoasPe +
+                "\nCapacidade total de pessoas em pé do ônibus: " + capPessoasPe +
+                "\nPassageiros que usaram bilhete normal: " + bilheteNormal +
+                "\nPassageiros que usaram bilhete de idoso: " + bilheteIdoso +
+                "\nPassageiros que usaram bilhete de estudante: " + bilheteEstudante +
+                "\nTotal de bihetes usados até o momento:" + totalBilhetes +
+                "\nTotal de voltas do ônibus: " + voltas
+                ;
+    }
 }
